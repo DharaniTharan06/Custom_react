f2381e188e48b939177af6095d5ba2d335f2fc33
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-08-24 19:42:44 +0530

    Fixed the dom creation method

--- customreact.js
+++ customreact.js
@@ -1,9 +1,14 @@
 function customrender(reactElement,container){
     const dom = document.createElement(reactElement.type)
     dom.innerHTML = reactElement.Children
+    /*
     dom.setAttribute('href',reactElement.props.href)
     dom.setAttribute('target',reactElement.props.target)
-
+    this method is not optimized*/
+    for(const prop in reactElement.props){
+        if(prop==children) continue
+        dom.setAttribute(prop,reactElement.props[prop])
+    }
     container.appendChild(dom)
 }
 
@@ -13,7 +18,7 @@ const  reactElement = {
         href: "https://google.com",
         target: '_blank'
     },
-    Children: 'Click me to visit google'
+    children: 'Click me to visit google'
 }
 const container = document.getElementById('root');
 
