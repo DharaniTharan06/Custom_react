bcd685fa96fc729562ff1a54f94eb65d84f5c840
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-08-24 19:35:09 +0530

    Added a custom reactElement to render the customdom

--- customreact.js
+++ customreact.js
@@ -1,2 +1,21 @@
-let container = document.getElementById('root');
-container.querySelector('h1').innerText = "asdf";
+function customrender(reactElement,container){
+    const dom = document.createElement(reactElement.type)
+    dom.innerHTML = reactElement.Children
+    dom.setAttribute('href',reactElement.props.href)
+    dom.setAttribute('target',reactElement.props.target)
+
+    container.appendChild(dom)
+}
+
+const  reactElement = {
+    type : 'a',
+    props:{
+        href: "https://google.com",
+        target: '_blank'
+    },
+    Children: 'Click me to visit google'
+}
+const container = document.getElementById('root');
+
+customrender(reactElement,container)
+
